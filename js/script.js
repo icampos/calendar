528a8e59425ec29eac1dc552135a907b3c479a92
icampos <isaac.camposcruz@gmail.com>
2017-03-09 15:11:26 -0600

    Populate calendar with initial date

--- js/script.js
+++ js/script.js
@@ -4,15 +4,51 @@ $(document).ready(function () {
 
 		$("#datepicker").datepicker({
 			changeYear: true,
-			changeMonth: true
+			changeMonth: true,
+			dateFormat: 'm/d/yy'
 		});
 
+		var startDate = function () {
+			date = $('#datepicker').val();
+			return date;
+
+		}
+
+		var renderCalendar = function (start, days, code) {
+
+			var initialDate,
+			minDate
+
+			initialDate = start;
+			minDate = initialDate.replace(',', '/');
+		
+
+			$('.result').datepicker("setDate", new Date(initialDate));
+			$(".result").datepicker({
+				changeYear: false,
+				changeMonth: false,
+				dateFormat: 'm/d/yy',
+				minDate: minDate,
+				
+			});
+
+		};
+
+		return {
+			renderCalendar: renderCalendar
+		};
+
 	})();
+	$('.render').click(function (e) {
 
-	var startDate = function () {
-		date = $('#datepicker').val();
-		return date;
+		start = $('#datepicker').val();
+		limit = parseInt($('#days').val());
+		code = $('#country').val();
 
-	}
+		Calendar.renderCalendar(start, limit, code);
+		
 
+		e.preventDefault();
+
+	});
 });
